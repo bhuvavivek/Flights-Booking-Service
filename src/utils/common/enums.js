d41130e25af690ca45a991c29ef053322ff2d677
bhuvavivek <vivekpate26@gmail.com>
2024-06-15 19:54:35 +0530

    added booking model along with utils from flight service

---
+++ src/utils/common/enums.js
@@ -0,0 +1,15 @@
+const SEAT_TYPE = {
+    ECONOMY: 'economy',
+    BUSINESS: 'business',
+    FIRST_CLASS: 'first-class',
+    PREMIUM_ECONOMY: 'premium-economy'
+}
+
+const BOOKING_STATUS = {
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    INITIATED: 'initiated',
+    PENDING: 'pending'
+}
+
+module.exports = { SEAT_TYPE, BOOKING_STATUS }
---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,12 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        this.message = message;
+        this.statusCode = statusCode
+        this.explanation = message;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
+module.exports = AppError;
